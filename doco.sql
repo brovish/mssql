f70d225051c42c28b5d8362e26bc0e0076d79a8a
ramneek singh <ramneekm@gmail.com>
2017-04-04 06:55:41 +1000

    sql doco

---
+++ doco.sql
@@ -0,0 +1,26 @@
+﻿--relational schema is the result of normalization
+--only one value in each column. you might have to repeat rows to get that(First normal form)
+--if a column has duplicate data, then it might be worthwhile to extract that data to a separate table and join with a foreign
+--key relationship. the foreign key can either be placed in the original table or we can again create a separate mapping table
+-- that will just associate the primary keys from both tables(second normal form)
+--all the columns in the table should depend on the primary key of that table(third normal forms)
+
+--collation refers to the character set and sort order
+--mdf extension is used for data files...it is just an convention..no need for .mdf extension and ldf extension is used for transaction 
+--log files...for fault tolerance, the mdf and ldf files should be on separate disks
+CREATE DATABASE [MVADemoLocalDB]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'MVADemoLocalDB', FILENAME = N'C:\SampleMSSQLDBs\data\MVADemoLocalDB.mdf' , SIZE = 4096KB , FILEGROWTH = 1024KB )
+ LOG ON 
+( NAME = N'MVADemoLocalDB_log', FILENAME = N'C:\SampleMSSQLDBs\logs\MVADemoLocalDB_log.ldf' , SIZE = 1024KB , FILEGROWTH = 10%)
+GO
+
+--sales is the schema and product is the table. schema is a way to organize db objects(tables, views, stored procedures). sort of a namespace fo db
+--identity is a auto incrementing column. primary key(composed of one or more columns) uniquely identifies data rows in the table. there can be 
+--only one primary key and identity in a table
+--varchar is varying width characters...null specifies a nullable column
+create table Sales.Product
+(ProductID Integer Identity Primary Key,
+Name VarChar(20),
+Price Decimal Null);
