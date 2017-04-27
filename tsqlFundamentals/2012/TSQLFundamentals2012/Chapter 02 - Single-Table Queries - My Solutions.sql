4fc25f6a6801a5cd1ef3de744a7c40014893410b
Ramneek Singh <ramneek.singh@csiro.au>
2017-04-28 06:47:52 +1000

    more sols

--- tsqlFundamentals/2012/TSQLFundamentals2012/Chapter 02 - Single-Table Queries - My Solutions.sql
+++ tsqlFundamentals/2012/TSQLFundamentals2012/Chapter 02 - Single-Table Queries - My Solutions.sql
@@ -41,10 +41,24 @@ USE "TSQL2012";
 --ORDER BY avgfreight DESC
 --OFFSET 0 ROWS FETCH NEXT 3 ROWS ONLY;
 
-SELECT 
-SORD.shipcountry, AVG(SORD.freight) AS avgfreight
-FROM Sales.Orders AS SORD
-WHERE SORD.orderdate >= '20070101' AND SORD.orderdate< '20080101'
-GROUP BY SORD.shipcountry
-ORDER BY avgfreight DESC
-OFFSET 0 ROWS FETCH NEXT 3 ROWS ONLY;
+--SELECT 
+--SORD.custid, SORD.orderdate,SORD.orderid,ROW_NUMBER() OVER(PARTITION BY SORD.custid ORDER BY SORD.orderdate, SORD.orderid)
+--FROM Sales.Orders AS SORD
+--ORDER BY SORD.custid
+
+--SELECT
+--E.empid,E.firstname,E.lastname,E.titleofcourtesy, 
+--CASE  E.titleofcourtesy
+--	WHEN 'Ms.' THEN 'Female'
+--	WHEN 'MRs.' THEN 'Female'
+--	WHEN 'Mr.' THEN 'Male'
+--	ELSE 'Unknown'
+--END
+--FROM HR.Employees AS E
+
+SELECT
+C.custid, C.region
+FROM SALES.Customers AS C
+ORDER BY C.region
+
+
