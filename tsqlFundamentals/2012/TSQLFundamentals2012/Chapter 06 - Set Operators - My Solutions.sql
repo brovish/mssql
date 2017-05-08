6be820e6012724a2a14ab9d85faacc125bd085e9
Ramneek Singh <ramneekm@gmail.com>
2017-05-08 15:32:47 +1000

    sols complete

--- tsqlFundamentals/2012/TSQLFundamentals2012/Chapter 06 - Set Operators - My Solutions.sql
+++ tsqlFundamentals/2012/TSQLFundamentals2012/Chapter 06 - Set Operators - My Solutions.sql
@@ -1,3 +1,66 @@
 USE "TSQL2012";
 GO
 
+--SELECT 1 AS n
+--UNION ALL
+--SELECT 2 
+--UNION ALL
+--SELECT 3 
+--UNION ALL
+--SELECT 4 
+--UNION ALL
+--SELECT 5 
+--UNION ALL
+--SELECT 6 
+--UNION ALL
+--SELECT 7 
+--UNION ALL
+--SELECT 8 
+--UNION ALL
+--SELECT 9 
+--UNION ALL
+--SELECT 10
+
+--SELECT 
+--O.custid,O.empid
+--FROM Sales.Orders AS O
+--WHERE O.orderdate>= DATEFROMPARTS(2008,01,01) AND O.orderdate< DATEFROMPARTS(2008,02,01)
+--EXCEPT
+--SELECT 
+--O.custid,O.empid
+--FROM Sales.Orders AS O
+--WHERE O.orderdate>= DATEFROMPARTS(2008,02,01) AND O.orderdate< DATEFROMPARTS(2008,03,01)
+
+--SELECT 
+--O.custid,O.empid
+--FROM Sales.Orders AS O
+--WHERE O.orderdate>= DATEFROMPARTS(2008,01,01) AND O.orderdate< DATEFROMPARTS(2008,02,01)
+--INTERSECT 
+--SELECT 
+--O.custid,O.empid
+--FROM Sales.Orders AS O
+--WHERE O.orderdate>= DATEFROMPARTS(2008,02,01) AND O.orderdate< DATEFROMPARTS(2008,03,01)
+
+--(
+--SELECT 
+--O.custid,O.empid
+--FROM Sales.Orders AS O
+--WHERE O.orderdate>= DATEFROMPARTS(2008,01,01) AND O.orderdate< DATEFROMPARTS(2008,02,01)
+
+--INTERSECT 
+--SELECT 
+--O.custid,O.empid
+--FROM Sales.Orders AS O
+--WHERE O.orderdate>= DATEFROMPARTS(2008,02,01) AND O.orderdate< DATEFROMPARTS(2008,03,01)
+--)
+--EXCEPT
+
+--SELECT 
+--O.custid,O.empid
+--FROM Sales.Orders AS O
+--WHERE O.orderdate>= DATEFROMPARTS(2007,01,01) AND O.orderdate< DATEFROMPARTS(2008,01,01)
+
+SELECT 
+*
+FROM 
+
