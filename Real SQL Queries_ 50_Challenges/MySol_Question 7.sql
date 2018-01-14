d98411cb0e72fa272ff5a97007b3aa9c45a29a89
Ramneek Singh <ramneekm@gmail.com>
2018-01-15 10:45:29 +1100

    sols

--- Real SQL Queries_ 50_Challenges/MySol_Question 7.sql
+++ Real SQL Queries_ 50_Challenges/MySol_Question 7.sql
@@ -7,5 +7,21 @@ WHERE SH.OrderDate >= '20110101' AND SH.OrderDate < '20120101'
 GROUP BY EMP.LoginID, ST.Name
 HAVING ST.Name IN ('NorthWest','Southwest', 'Canada')
 
---SELECT *
---FROM Sales.SalesOrderHeader
+DROP TABLE #TOPSALESPERSON
+--with salary
+SELECT  PR.FirstName,PR.LastName, ST.Name, SUM(SH.SubTotal) AS TOTALSALES
+INTO #TOPSALESPERSON
+FROM Sales.SalesPerson AS SP INNER JOIN Sales.SalesTerritory AS ST ON ST.TerritoryID = SP.TerritoryID  INNER JOIN Sales.SalesOrderHeader AS SH ON SH.SalesPersonID = SP.BusinessEntityID INNER JOIN
+Person.Person AS PR ON PR.BusinessEntityID = SP.BusinessEntityID
+WHERE SH.OrderDate >= '20110101' AND SH.OrderDate < '20120101' 
+GROUP BY PR.FirstName, PR.LastName, ST.Name
+HAVING ST.Name IN ('NorthWest','Southwest', 'Canada')
+
+ALTER TABLE #TOPSALESPERSON
+	ADD SALARY MONEY
+
+
+UPDATE #TOPSALESPERSON
+	SET SALARY = CASE WHEN LastName = 'CAMPBELL' THEN 79500 WHEN LastName = 'CAMPBELL' THEN 60000
+						WHEN LastName = 'CAMPBELL' THEN 59500  WHEN LastName = 'CAMPBELL' THEN 56000 
+						WHEN LastName = 'CAMPBELL' THEN 680000  WHEN LastName = 'CAMPBELL' THEN 80000 END
