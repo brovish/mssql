9c2ab9b2669d4190c238a6820f9fde92b8633e21
ramneek singh <ramneekm@gmail.com>
2018-01-28 19:31:21 +1100

    tatt

--- Real SQL Queries_ 50_Challenges/2015/MySol_Question 40.sql
+++ Real SQL Queries_ 50_Challenges/2015/MySol_Question 40.sql
@@ -1,9 +1,27 @@
 USE AdventureWorks2012;
 
-SELECT CASE 
-			WHEN SOH.SubTotal + SOH.Freight + SOH.TaxAmt < 1000 THEN 1  
-			WHEN SOH.SubTotal + SOH.Freight + SOH.TaxAmt < 5000 THEN 1 
-			ELSE 0
+SELECT 
+		CASE 
+			WHEN SOH.SubTotal + SOH.Freight + SOH.TaxAmt < 1000 THEN 1
+			WHEN SOH.SubTotal + SOH.Freight + SOH.TaxAmt >1000 AND SOH.SubTotal + SOH.Freight + SOH.TaxAmt < 5000 THEN 2
+			ELSE 3
 		END
+		,CASE 
+			WHEN SOH.SubTotal + SOH.Freight + SOH.TaxAmt < 1000 THEN 'LESS THAN 1000'
+			WHEN SOH.SubTotal + SOH.Freight + SOH.TaxAmt >1000 AND SOH.SubTotal + SOH.Freight + SOH.TaxAmt < 5000 THEN 'LESS THAN 5000'
+			ELSE 'MORE THAN 5000'
+		END
+		,COUNT(*)
 FROM Sales.SalesOrderHeader AS SOH
 WHERE SOH.OrderDate >= '20120101' AND SOH.OrderDate < '20130101'
+GROUP BY
+		CASE 
+			WHEN SOH.SubTotal + SOH.Freight + SOH.TaxAmt < 1000 THEN 1
+			WHEN SOH.SubTotal + SOH.Freight + SOH.TaxAmt >1000 AND SOH.SubTotal + SOH.Freight + SOH.TaxAmt < 5000 THEN 2
+			ELSE 3
+		END
+		,CASE 
+			WHEN SOH.SubTotal + SOH.Freight + SOH.TaxAmt < 1000 THEN 'LESS THAN 1000'
+			WHEN SOH.SubTotal + SOH.Freight + SOH.TaxAmt >1000 AND SOH.SubTotal + SOH.Freight + SOH.TaxAmt < 5000 THEN 'LESS THAN 5000'
+			ELSE 'MORE THAN 5000'
+		END
