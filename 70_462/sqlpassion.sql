ca3e9b01ed39691b19160c68cac832ce03011dad
ramneek singh <ramneekm@gmail.com>
2018-05-17 19:22:48 +1000

    tatt

---
+++ 70_462/sqlpassion.sql
@@ -0,0 +1,66 @@
+--create database testdb
+--go
+
+--use testdb
+--go
+--create table tbl
+--(
+--	firstname char(50),
+--	lastname char(50),
+--	address char(100)
+--)
+--go
+
+--insert into tbl values
+--('ramneek','singh','sdfdsghgfh 32 esdfsdf,ewr')
+--go 2
+
+
+dbcc traceon(3604)--need for dbcc page command
+--find which data pages belong to our tbl. columns PageFID and PagePID.
+--this will return 2 pages: IAM page with pagetype 10 and data page
+dbcc ind(testdb,tbl,-1)
+
+--remember page header size is 96 bytes, then we have payload and then the rowoffset array(aslot array).
+--row offset array need 2 bytes to store location for each record
+--dump the first data page using FID and PID of the page. First record in data pages are at offset 96
+dbcc page(testdb,1,312,3)
+dbcc page(testdb,1,312,1)
+dbcc page(testdb,1,312,2)
+
+
+dbcc traceon(3604)
+go
+
+/* both GAM and SGAM use bit 1 for denoting free but how can we tell if a uniform extent has free page?:
+current use of extent| GAM bit setting| SGAM bit setting
+---------------------  ---------------  ----------------
+free, not in use		1				0
+
+uniform extent			0				0
+or full mixed extent
+
+mixed extent with		0				1
+free pages
+
+*/
+--1rst interval. GAM page records which extents have been allocated for any type of use. 1 for extent that is free and 0 for in use.
+--excluding the page header and other overhead, it has about 8000 bytes or 64000 bits for use or. that means it can store usage for 64000 extents 
+--or for 512000 pages or about 4 gb of memory. So 1 GAM page bitmaps' extents totalling about 4 GB of memory. So when our database file is larger than
+--4 GB, SQL server needs multiple GAM pages. First GAM page is always the 3rd page and the next is after 511230 pages(about 4 GB)
+--gam(1:2)
+--dumping out GAM page which is always the 3 page from start(page id is 0 based, so we pass 2) and the display mode is 3 for database tpc_e
+dbcc page(tpc_e,1,2,3)
+
+--A SGAM page again looks after 4 GB of memory. It records which extents are used as mixed extents and have atleast 1 unused page(how do we track 
+--which uniform extents have free pages?). If bit is 1, that extent is a mixed extent and has free a page. If 0, the extent either is not a mixed 
+--extent or is a mixed extent with no free pages. First SGAM page is always the 4rd page and the next is after 511230 pages(about 4 GB)
+--sgam(1:3)
+--dumping out SGAM page which is always the 4 page from start(page id is 0 based, so we pass 3) and the display mode is 3 for database tpc_e
+dbcc page(tpc_e,1,3,3)
+
+--2nd interval. the second GAM and SGAM page occurs after 511230 pages from the first one.
+dbcc page(tpc_e,1,511232,3)
+
+dbcc page(tpc_e,1,511233,3)
+
