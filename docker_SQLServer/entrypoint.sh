853252b784f7183c314b7ffb61472d32646512f9
Ramneek Singh <ramneekm@gmail.com>
2020-05-22 15:56:48 +1000

    stuff

--- docker_SQLServer/entrypoint.sh
+++ docker_SQLServer/entrypoint.sh
@@ -1,1 +1,4 @@
+#we have to reference the executable/script that we want to run when docker image is run.
+#That means we can only import data by running sql scripts after the server is executing. So the following 
+#is wrong in wrong order.
 /usr/work/import-data.sh & /opt/mssql/bin/sqlservr
