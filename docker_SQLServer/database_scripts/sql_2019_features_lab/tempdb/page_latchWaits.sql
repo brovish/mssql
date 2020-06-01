9b49f47949244306e21f91cfc353a31faa6a678e
Ramneek Singh <ramneekm@gmail.com>
2020-06-01 15:12:14 +1000

    tatt

--- docker_SQLServer/database_scripts/sql_2019_features_lab/tempdb/page_latchWaits.sql
+++ docker_SQLServer/database_scripts/sql_2019_features_lab/tempdb/page_latchWaits.sql
@@ -1,8 +1,18 @@
 USE tempdb;
 GO
-SELECT object_name(page_info.object_id), page_info.*
+SELECT d.status, object_name(page_info.object_id), page_info.*
 FROM sys.dm_exec_requests AS d
   CROSS APPLY sys.fn_PageResCracker(d.page_resource) AS r
   CROSS APPLY sys.dm_db_page_info(r.db_id, r.file_id, r.page_id,'DETAILED')
     AS page_info;
 GO
+
+--you should get 3 types of exec requests: suspended, runnable, running
+SELECT distinct d.status
+FROM sys.dm_exec_requests AS d
+  CROSS APPLY sys.fn_PageResCracker(d.page_resource) AS r
+  CROSS APPLY sys.dm_db_page_info(r.db_id, r.file_id, r.page_id,'DETAILED')
+    AS page_info;
+GO
+
+
--- docker_SQLServer/database_scripts/sql_2019_features_lab/tempdb/tempdb.sql
+++ docker_SQLServer/database_scripts/sql_2019_features_lab/tempdb/tempdb.sql
@@ -21,16 +21,26 @@ GO
 --step 2: creae perfmon counters (refer perfmon_counters.png)
 
 -- Step 3: And then stress test the db and observe the perfmon counters. we are creating a temp table in the sp
--- and stess testing it (execution context/scope of the temp table would be different for different execution calls hence same name temp table
--- would be created in temp db??)TODO: difference bw sessions and scope is what explains it. write about it
+-- and stess testing it.
 
 -- ostress runs fine in the cmd window but not in powershell. haven't analysed must be some command syntax diff
 -- C:\Users\sin17h\Documents\sql-server-samples\samples\features\in-memory-database\memory-optimized-tempdb-metadata>ostress.exe -Slocalhost,1401 -USA -Ppassw0rd1! -Q"Select name from sys.databases" -n1 -r1
 -- ostress -Slocalhost,1401 -USA -Ppassw0rd1! -Q"exec letsgomavs" -n50 -r5000 -dDallasMavericks
 -- Repeat step 2 on local as not sure how to connect perfmon to sql in docker
 -- ostress -SINTERVAL-BM\sql2019 -E -Q"exec letsgomavs" -n50 -r5000 -dDallasMavericks --this uses windows auth 
+
 ---- if you run the Create table command directly (without wrapping it in a SP), it will throw an error that the object #gomavs already exists
--- ostress -SINTERVAL-BM\sql2019 -E -Q"USE DallasMavericks;GO CREATE TABLE #gomavs (col1 INT);" -n50 -r5000 -dDallasMavericks 
+---- this can be explained based on sessions and scope.
+-- ostress -SINTERVAL-BM\sql2019 -E -Q"USE DallasMavericks; CREATE TABLE #gomavs (col1 INT);" -n50 -r5000 -dDallasMavericks 
+
+---- works fine beause each of the 500 threads starts a new session and in each session we create a temp table with same name.
+--ostress -SINTERVAL-BM\sql2019 -E -Q"USE DallasMavericks; CREATE TABLE #gomavs (col1 INT);" -n500 -r1
+---- where this fails saying "#gomavs already exists" because in the same session we create temp table twice with the same name.
+-- ostress -SINTERVAL-BM\sql2019 -E -Q"USE DallasMavericks; CREATE TABLE #gomavs (col1 INT);" -n1 -r2
+
+----this work fine even though for the same session (thread) we are calling the sp twice. And that is because the temp table we create is in  
+----the sp scoped by the sp. It only exists locally in the sp, not visible in the outer session context.
+-- ostress -SINTERVAL-BM\sql2019 -E -Q"exec letsgomavs" -n1 -r2 -dDallasMavericks 
 
 
 --step 4: run page_latchWaits.sql to see to what objects the majority of page latch wait belongs to. Run it while the stress testing
@@ -38,4 +48,16 @@ GO
 -- This system table is the primary table to store table metadata in the tempdb database 
 -- SQLCMD.EXE -S INTERVAL-BM\sql2019 -E -i page_latchWaits.sql
 
---step 5: 
+--step 5: Enable the Optimize Tempdb Metadata capability 
+-- SQLCMD.EXE -S INTERVAL-BM\sql2019 -E -i optimizetempdb.sql
+
+-- --step 6: restart the service
+-- net stop MSSQL$SQL2019 --this works on cmd admin
+-- net stop 'MSSQL$SQL2019' --this works in powershell.
+-- net start MSSQL$SQL2019
+
+-- --step 7: run stress test from step 3 again. It took just 10 seconds to complete the stress test. I alo ran the page_latchWaits.sql while 
+-- the stress test was running and it did not show any wait 
+-- 06/01/20 14:29:21.630 [0x00006898] OSTRESS exiting normally, elapsed time: 00:00:10.802
+----where as before it took 32 secs
+-- 06/01/20 14:33:38.383 [0x00001EC8] OSTRESS exiting normally, elapsed time: 00:00:32.407
