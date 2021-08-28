27e9dccbf11792d1589e89fccdba7e5738b5fb7d
Ramneek Singh <ramneekm@gmail.com>
2021-08-29 07:07:45 +1000

    dynamic pivot generation stored procedure

--- FunSQLQueries/dynamicPivot/dyn_pivot_sp.sql
+++ FunSQLQueries/dynamicPivot/dyn_pivot_sp.sql
@@ -18,7 +18,7 @@ as
 		--input validation for sql-injection, etc. goes here. The problem it is difficult to validate against sql-injection
 
 		declare @sql as nvarchar(max), 
-				@cols as nvarchar(max) = N'', 
+				@cols as nvarchar(max),-- = N'', 
 				@newline as nvarchar(2) = nchar(13) + nchar(10);
 
 		if coalesce(object_id(@query, N'u'), object_id(@query, N'v')) is not null
@@ -31,32 +31,10 @@ as
 		if @agg_col = N'*'
 			set @agg_col = N'1';
 
-		--now construct the comma separate list of quoted column values from the @on_col column
-		--set @sql = N'declare @col_values as nvarchar(max) = N''''; select @col_values += concat('','',  quotename(pivotColVal) )
-		--				from (select distinct ' + @on_col + ' as pivotColVal
-		--						from ' + @query + ') as distinctPivotColValues;'; 
-		set @sql = N'print concat(''The values are:'', @col_values) ;select @col_values += concat('','',  quotename(pivotColVal) )
-						from (select distinct ' + @on_col + ' as pivotColVal
-								from ' + @query + ') as distinctPivotColValues;'; 
-								
-		print @sql;--this generated sql statement is correct. But the @col_values output parameter is returned null??
-		exec sp_executesql @stmt= @sql, @params = N'@col_values as nvarchar(max) = N'' '' output', @col_values = @cols output
-		select @cols, 'works 1';
+		--now construct the comma separate list of quoted column values from the @on_col column. Here i try out 3 different approaches(differ in only the way we pass parameter) in order to 
+		--avoid using STUFF to generate the comma separated column list:
 
-		--declare @col_values_outerscope As NVARCHAR(MAX)= N'';
-		--set @sql = N'print concat(''The values are:'''+ @cols + ') ;select '+ @cols +' += concat('','',  quotename(pivotColVal) )
-		--				from (select distinct ' + @on_col + ' as pivotColVal
-		--						from ' + @query + ') as distinctPivotColValues;'; 
-
-		set @sql = N'select '+ @cols +' += concat('','',  quotename(pivotColVal) )
-				from (select distinct ' + @on_col + ' as pivotColVal
-						from ' + @query + ') as distinctPivotColValues;'; 
-								
-		print @sql;--this generated sql statement is correct. But the @col_values output parameter is returned null??
-		--exec sp_executesql @stmt= @sql
-		select @cols, 'works 2';
-
-
+		----Approach 1: Use STUFF. Avoid it as it is not very readable. MAybe it is more perfromance. TODO: Benchmark the performance
 		--  SET @sql =
 		--		N'SET @col_values = '                                + @newline +
 		--		N'  STUFF('                                          + @newline +
@@ -70,9 +48,92 @@ as
 		--		N'     FOR XML PATH('''')),'+ @newline +
 		--		N'    1, 1, N'''');'
 		
-		--print @sql;--this generated sql statement is correct. But the @col_values output parameter is returned null??
+		--print @sql;
 		--exec sp_executesql @stmt= @sql, @params = N'@col_values as nvarchar(max) output', @col_values = @cols output
-		--select @cols;
+		--select @cols, 'Works 1';
+
+		--uncomment each of these approaches on-by-one to see their output. Don't uncomment all at the same time
+		----Approach 2: 
+		----2.1 Commentary: The Parameter (@col_values) and the Argument (@cols) being passed to it are both not instantiated. So the starting value of @col_value is NULL
+		---- and concatenating with NULL results in NULL
+		--set @sql = N'select @col_values += concat('','',  quotename(pivotColVal) )
+		--				from (select distinct ' + @on_col + ' as pivotColVal
+		--						from ' + @query + ') as distinctPivotColValues;'; 
+
+		--print @sql;
+		--exec sp_executesql @stmt= @sql, @params = N'@col_values as nvarchar(max) output', @col_values = @cols output
+		--select @cols, 'Does not work 2.1';
+
+		----2.2 Commantary: Lets instantiate the argument being passed (to the OUTPUT parameter) to a empty string. It works because not only would the value of @col_values gets linked to @cols (as is expected)
+		----but other way around as well. So instantiating @cols to a value also set that value to @col_values when the dynamic query starts executing
+		--set @cols = N'';
+		--set @sql = N'select @col_values += concat('','',  quotename(pivotColVal) )
+		--				from (select distinct ' + @on_col + ' as pivotColVal
+		--						from ' + @query + ') as distinctPivotColValues;'; 
+
+		--print @sql;
+		--exec sp_executesql @stmt= @sql, @params = N'@col_values as nvarchar(max) output', @col_values = @cols output
+		--select @cols, 'works 2.2';
+
+		----Approach 3:
+		----3.1 Commentary: You cannot declare @col_values inside the dynamic query as the variable is already declared outside as a OUTPUT parameter
+		--set @sql = N'declare @col_values as nvarchar(max) = N''''; select @col_values += concat('','',  quotename(pivotColVal) )
+		--				from (select distinct ' + @on_col + ' as pivotColVal
+		--						from ' + @query + ') as distinctPivotColValues;'; 
+
+		--print @sql;
+		--exec sp_executesql @stmt= @sql, @params = N'@col_values as nvarchar(max) output', @col_values = @cols output
+		--select @cols, 'Does not work 3.1';
+
+		----3.2 Commentary: This would work as we instantiate the parameter @col_values inside the dynamic query.
+		--set @sql = N'set @col_values = N''''; select @col_values += concat('','',  quotename(pivotColVal) )
+		--				from (select distinct ' + @on_col + ' as pivotColVal
+		--						from ' + @query + ') as distinctPivotColValues;'; 
+
+		--print @sql;
+		--exec sp_executesql @stmt= @sql, @params = N'@col_values as nvarchar(max) output', @col_values = @cols output
+		--select @cols, 'Works 3.2';
+
+		----3.3 Commentary: This does not work even though we instantiate the parameter @col_values when we declare it outside the dynamic query.
+		--set @sql = N'select @col_values += concat('','',  quotename(pivotColVal) )
+		--				from (select distinct ' + @on_col + ' as pivotColVal
+		--						from ' + @query + ') as distinctPivotColValues;'; 
+
+		--print @sql;
+		--exec sp_executesql @stmt= @sql, @params = N'@col_values as nvarchar(max) = N'' ''  output', @col_values = @cols output
+		--select @cols, 'Does not work 3.3';
+
+		----If you want to debug the @col_values inside the dynamic query, print it to show that it is NULL
+		--set @sql = N'print concat(''The values are:'', @col_values) ;select @col_values += concat('','',  quotename(pivotColVal) )
+		--				from (select distinct ' + @on_col + ' as pivotColVal
+		--						from ' + @query + ') as distinctPivotColValues;'; 
+								
+		--print @sql;--this generated sql statement is correct. But the @col_values output parameter is returned null??
+		--exec sp_executesql @stmt= @sql, @params = N'@col_values as nvarchar(max) = N'' '' output', @col_values = @cols output
+		--select @cols, 'Does not work 3.3. Debug the @col_values by printing it when the query is executed. Check the Messages tab for printed output';
+
+		--Approach 4:
+		----4.1 Commentary: We can also remove parameterization from the query and directly use @cols in the dynamic query This won't work as @cols is uninstantiated. So 
+		----concatenating with NULL will result in NULL
+		--set @sql = N'select '+ @cols +' += concat('','',  quotename(pivotColVal) )
+		--		from (select distinct ' + @on_col + ' as pivotColVal
+		--				from ' + @query + ') as distinctPivotColValues;'; 
+								
+		--print @sql;--this generated sql statement is correct. But the @col_values output parameter is returned null??
+		--exec sp_executesql @stmt= @sql
+		--select @cols, 'Does not work 4.1';
+
+		--4.2 Commentary: We can also remove parameterization from the query and directly use @cols in the dynamic query This won't work as @cols is uninstantiated. So 
+		--concatenating with NULL will result in NULL
+		set @cols = N'';
+		set @sql = N'select '+ @cols +' += concat('','',  quotename(pivotColVal) )
+				from (select distinct ' + @on_col + ' as pivotColVal
+						from ' + @query + ') as distinctPivotColValues;'; 
+								
+		print @sql;--this generated sql statement is correct. But the @col_values output parameter is returned null??
+		exec sp_executesql @stmt= @sql
+		select @cols, 'Does not work 4.2';
+
 
 		--select 1;
 	end try	
