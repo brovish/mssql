9fe56377a2b2fd65f031f4075b778aeb33759865
Ramneek Singh <ramneekm@gmail.com>
2021-08-29 08:59:47 +1000

    dynamic pivot generation stored procedure

--- FunSQLQueries/dynamicPivot/dyn_pivot_sp.sql
+++ FunSQLQueries/dynamicPivot/dyn_pivot_sp.sql
@@ -11,15 +11,15 @@ go
 --dynamic query, we can embed print statement inside it. Refer approach 3.3
 
 create or alter proc dbo.sp_dynPivot
-	@query as nvarchar(max),--table or view to be queried
-	@on_rows as nvarchar(max),	
-	@on_col as nvarchar(max),
+	@base_table as nvarchar(max),--table or view to be queried
+	@on_rows as nvarchar(max),--the 'For' column or the GROUP BY key column. The @agg_func works for each unique value for this key for spreading.
+	@on_col as nvarchar(max),--the 'Categorical' column. The column whose distict values become the new columns
 	@agg_func as nvarchar(max) = N'MAX',
-	@agg_col as nvarchar(max)
+	@agg_col as nvarchar(max)--The 'Data' Column. The col on which we apply the @agg_func to compute the values for newly generated columns.
 as
 	begin try
 		--validate the input
-		if @query is null or @on_rows is null or @on_col is null or @agg_func is null or @agg_col is null
+		if @base_table is null or @on_rows is null or @on_col is null or @agg_func is null or @agg_col is null
 			throw 50001, 'Invalid input paramaeters.', 1;
 
 		--input validation for sql-injection, etc. goes here. The problem it is difficult to validate against sql-injection
@@ -28,17 +28,17 @@ as
 				@cols as nvarchar(max),-- = N'', 
 				@newline as nvarchar(2) = nchar(13) + nchar(10);
 
-		if coalesce(object_id(@query, N'u'), object_id(@query, N'v')) is not null
-			set @query = N'select * from ' + @query;--should have used concat
+		--Construct a derived table query from the base table so that it goes in the FROM section of pivot template. Also provide an ALIAS for it.
+		if coalesce(object_id(@base_table, N'u'), object_id(@base_table, N'v')) is not null
+			set @base_table = N'select * from ' + @base_table;--should have used concat
 
-		--now since this is the base table query, it has to be made a derived table query so that it goes in the FROM section of pivot template. Also provide an ALIAS for it
-		set @query = N'(' + @query + ') as baseTableQuery';
+		set @base_table = N'(' + @base_table + ') as baseTableQuery';
 
 		--if the user passes '*' in @agg_col parameter, use column number '1' for aggregation
 		if @agg_col = N'*'
 			set @agg_col = N'1';
 
-		--now construct the comma separate list of quoted column values from the @on_col column. Here i try out 3 different approaches(differ in only the way we pass parameter) in order to 
+		--now construct the comma separate list of quoted column values from the @on_col column. Here i try out 3 different approaches in order to 
 		--avoid using STUFF to generate the comma separated column list:
 
 		----Approach 1: Use STUFF. Avoid it as it is not very readable. MAybe it is more perfromance. TODO: Benchmark the performance
@@ -50,7 +50,7 @@ as
 		--				 + 'N'']'' AS [text()]'                      + @newline +
 		--		N'     FROM (SELECT DISTINCT('
 		--				 + @on_col + N') AS pivot_col'              + @newline +
-		--		N'           FROM' + @query + N') AS DistinctCols'   + @newline +
+		--		N'           FROM' + @base_table + N') AS DistinctCols'   + @newline +
 		--		N'     ORDER BY pivot_col'+ @newline +
 		--		N'     FOR XML PATH('''')),'+ @newline +
 		--		N'    1, 1, N'''');'
@@ -59,13 +59,13 @@ as
 		--exec sp_executesql @stmt= @sql, @params = N'@col_values as nvarchar(max) output', @col_values = @cols output
 		--select @cols, 'Works 1';
 
-		--uncomment each of these approaches on-by-one to see their output. Don't uncomment all at the same time
+		--Uncomment each of these approaches on-by-one to see their output. Don't uncomment all at the same time
 		----Approach 2: 
 		----2.1 Commentary: The Parameter (@col_values) and the Argument (@cols) being passed to it are both not instantiated. So the starting value of @col_value is NULL
 		---- and concatenating with NULL results in NULL
 		--set @sql = N'select @col_values += concat('','',  quotename(pivotColVal) )
 		--				from (select distinct ' + @on_col + ' as pivotColVal
-		--						from ' + @query + ') as distinctPivotColValues;'; 
+		--						from ' + @base_table + ') as distinctPivotColValues;'; 
 
 		--print @sql;--generated sql statement is correct. But the @col_values output parameter is returned null as it was not instantiated (therefore NULL)
 		--exec sp_executesql @stmt= @sql, @params = N'@col_values as nvarchar(max) output', @col_values = @cols output
@@ -76,7 +76,7 @@ as
 		set @cols = N'';
 		set @sql = N'select @col_values += concat('','',  quotename(pivotColVal) )
 						from (select distinct ' + @on_col + ' as pivotColVal
-								from ' + @query + ') as distinctPivotColValues;'; 
+								from ' + @base_table + ') as distinctPivotColValues;'; 
 
 		print @sql;
 		exec sp_executesql @stmt= @sql, @params = N'@col_values as nvarchar(max) output', @col_values = @cols output
@@ -86,7 +86,7 @@ as
 		----3.1 Commentary: You cannot declare @col_values inside the dynamic query as a variable as it is already declared outside as a OUTPUT parameter
 		--set @sql = N'declare @col_values as nvarchar(max) = N''''; select @col_values += concat('','',  quotename(pivotColVal) )
 		--				from (select distinct ' + @on_col + ' as pivotColVal
-		--						from ' + @query + ') as distinctPivotColValues;'; 
+		--						from ' + @base_table + ') as distinctPivotColValues;'; 
 
 		--print @sql;
 		--exec sp_executesql @stmt= @sql, @params = N'@col_values as nvarchar(max) output', @col_values = @cols output
@@ -95,7 +95,7 @@ as
 		----3.2 Commentary: This would work as we instantiate the parameter @col_values inside the dynamic query.
 		--set @sql = N'set @col_values = N''''; select @col_values += concat('','',  quotename(pivotColVal) )
 		--				from (select distinct ' + @on_col + ' as pivotColVal
-		--						from ' + @query + ') as distinctPivotColValues;'; 
+		--						from ' + @base_table + ') as distinctPivotColValues;'; 
 
 		--print @sql;
 		--exec sp_executesql @stmt= @sql, @params = N'@col_values as nvarchar(max) output', @col_values = @cols output
@@ -104,7 +104,7 @@ as
 		----3.3 Commentary: This does not work even though we instantiate the parameter @col_values when we declare it outside the dynamic query.
 		--set @sql = N'select @col_values += concat('','',  quotename(pivotColVal) )
 		--				from (select distinct ' + @on_col + ' as pivotColVal
-		--						from ' + @query + ') as distinctPivotColValues;'; 
+		--						from ' + @base_table + ') as distinctPivotColValues;'; 
 
 		--print @sql;--generated sql statement is correct. But the @col_values output parameter is returned null as it was instantiated outside the dynamic query string which for some reason does not work.
 		--exec sp_executesql @stmt= @sql, @params = N'@col_values as nvarchar(max) = N'' ''  output', @col_values = @cols output
@@ -113,9 +113,9 @@ as
 		----If you want to debug the @col_values inside the dynamic query, print it to show that it is NULL
 		--set @sql = N'print concat(''The values are:'', @col_values) ;select @col_values += concat('','',  quotename(pivotColVal) )
 		--				from (select distinct ' + @on_col + ' as pivotColVal
-		--						from ' + @query + ') as distinctPivotColValues;'; 
+		--						from ' + @base_table + ') as distinctPivotColValues;'; 
 								
-		--print @sql;--this generated sql statement is correct. But the @col_values output parameter is returned null as it was not instantiated (therefore NULL)
+		--print @sql;--this generated sql statement is correct. But the @col_values output parameter is returned null as it was instantiated outside the dynamic query string which for some reason does not work.
 		--exec sp_executesql @stmt= @sql, @params = N'@col_values as nvarchar(max) = N'' '' output', @col_values = @cols output
 		--select @cols, 'Does not work 3.3. Debug the @col_values by printing it when the query is executed. Check the Messages tab for printed output';
 
@@ -123,19 +123,19 @@ as
 		----4.1 Commentary: This won't work as @cols is uninstantiated. So concatenating with NULL will result in NULL
 		--set @sql = N'select '+ @cols +' += concat('','',  quotename(pivotColVal) )
 		--		from (select distinct ' + @on_col + ' as pivotColVal
-		--				from ' + @query + ') as distinctPivotColValues;'; 
+		--				from ' + @base_table + ') as distinctPivotColValues;'; 
 								
 		--print @sql;--generated sql statement is wrong as value of @cols is embedded in the query, not the variable itself.
 		--exec sp_executesql @stmt= @sql
 		--select @cols, 'Does not work 4.1';
 
 		----4.2 Commentary: We can instantiate @cols to empty string but the problem here when @cols is already evaluated to its value before we execute the dynamic query. The print @sql statement shows
-		----that the empty string for @cols is embedded in the dynamic query as an value. But we want the @cols variable to be embedded, not its value. Contrast it with @on_col and @query variables whose value
+		----that the empty string for @cols is embedded in the dynamic query as an value. But we want the @cols variable to be embedded, not its value. Contrast it with @on_col and @base_table variables whose value
 		----we want to embed in the query, not the variable themselves (we cold have embed them as variables as well and it would also work). 
 		--set @cols = N'';
 		--set @sql = N'select '+ @cols +' += concat('','',  quotename(pivotColVal) )
 		--		from (select distinct ' + @on_col + ' as pivotColVal
-		--				from ' + @query + ') as distinctPivotColValues;'; 
+		--				from ' + @base_table + ') as distinctPivotColValues;'; 
 								
 		--print @sql;--generated sql statement is wrong as value of @cols is embedded in the query, not the variable itself.
 		--exec sp_executesql @stmt= @sql
@@ -149,9 +149,9 @@ as
 go
 
 exec dbo.sp_dynPivot 
-	@query = N'TSQLV3.Sales.Orders',
-	@on_rows = N'custid',
-	@on_col = N'year(orderdate)',
-	@agg_func = N'max',
-	@agg_col = N'val'
+	@base_table = N'TSQLV3.Sales.Orders',
+	@on_rows = N'custid',--'For' Column or the 'GROUP BY'key/columns 
+	@on_col = N'year(orderdate)',--'Categorical' column. Distinct values of this col become new columns
+	@agg_func = N'max',--the aggregate func
+	@agg_col = N'val'--'Data' column which is then aggregated
 
